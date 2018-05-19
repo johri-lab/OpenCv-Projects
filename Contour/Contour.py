fa4737fa8cc948916020be0499477528ebd626d1
johri002 <shikharjohri123@gmail.com>
2018-05-19 20:16:16 +0530

    OpenCv project to count objects in an image

---
+++ Contour/Contour.py
@@ -0,0 +1,34 @@
+import cv2
+import argparse
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to image")
+args = vars(ap.parse_args())
+
+img = cv2.imread(args["image"])
+
+#BGR to gray color
+gray = cv2.cvtColor(img.copy(),cv2.COLOR_BGR2GRAY)
+
+#apply a blur filter to remove noise
+blur = cv2.GaussianBlur(gray,(11,11),29)
+
+#apply thrashing
+ret, thresh = cv2.threshold(blur,235,255,cv2.THRESH_BINARY_INV)
+
+#displays thresholded image
+cv2.imshow("THRESHOLD", thresh)
+
+#find all contours in the processed image
+contours,heirarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+print "There are %d shapes in the image"%len(contours)
+
+#draw all contours 
+for i in contours: 
+	cv2.drawContours(img,[i],-1,(0,0,255),3)
+cv2.imwrite("Desktop/Contour/ShapeContoured.jpg",img)
+
+#display conoured image
+cv2.imshow("Contoured Image",img)
+cv2.waitKey(0)
