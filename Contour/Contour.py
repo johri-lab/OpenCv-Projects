218c5397ffbb0486d0225bba5edc77891e416e96
Shikhar Johri <30645315+johri002@users.noreply.github.com>
2018-05-20 02:28:33 +0530

    Update Contour.py

--- Contour/Contour.py
+++ Contour/Contour.py
@@ -8,26 +8,26 @@ args = vars(ap.parse_args())
 
 img = cv2.imread(args["image"])
 
-#BGR to gray color
+# BGR to gray color conversion
 gray = cv2.cvtColor(img.copy(),cv2.COLOR_BGR2GRAY)
 
-#apply a blur filter to remove noise
+# apply blur filter to remove noise
 blur = cv2.GaussianBlur(gray,(11,11),29)
 
-#apply thrashing
+# apply thrasholding
 ret, thresh = cv2.threshold(blur,235,255,cv2.THRESH_BINARY_INV)
 
-#displays thresholded image
+# display thresholded image
 cv2.imshow("THRESHOLD", thresh)
 
-#find all contours in the processed image
+# find all contours in the processed image
 contours,heirarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 print "There are %d shapes in the image"%len(contours)
 
-#draw all contours 
+# draw all contours( outlining ) 
 for i in contours: 
 	cv2.drawContours(img,[i],-1,(0,0,255),3)
 
-#display conoured image
+# display the contoured image
 cv2.imshow("Contoured Image",img)
 cv2.waitKey(0)
