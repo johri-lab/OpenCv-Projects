2a75a11673074aa3732bdaa7ee311b56e3c1e9c9
Shikhar Johri <30645315+johri002@users.noreply.github.com>
2018-11-26 17:02:52 +0530

    Update histofin.py

--- Histogram/histofin.py
+++ Histogram/histofin.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img=cv2.imread('leaf5.jpg',0)
+img=cv2.imread('histo.png',0)
 
 #resize if images are large in sizes
 #img = cv2.resize(img, (img.shape[1],img.shape[0]))
